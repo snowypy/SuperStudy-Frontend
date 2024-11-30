02b57192c5218800fb575fa30452c7ec79731ab5
Cigan <git@snowyjs.lol>
2024-11-30 00:20:24 +0000

    Delete backend directory

--- backend/src/main/kotlin/org/superstudy/backend/v1/backend/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.superstudy.backend.v1.backend.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.superstudy.backend.v1.backend.model.User
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-}
