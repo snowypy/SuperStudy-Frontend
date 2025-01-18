b8eb94dfeb830436ba0d72f4f9f26f8fe953ef23
snowypy <git@snowyjs.lol>
2025-01-18 12:42:17 +0000

    use Monaspace Neon font :)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import './global-styles.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/lib/theme-provider'
+import localFont from 'next/font/local';
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 
@@ -12,6 +13,17 @@ export const metadata = {
     description: 'Learn quick + fast ❤️',
 }
 
+const monaspacekrypton = localFont({
+    src: [
+        {
+            path: '../public/fonts/MonaspaceNeon-Regular.woff',
+            weight: '400',
+            style: 'normal',
+        }
+    ],
+    variable: '--font-krypton'
+});
+
 export default function RootLayout({
     children
 }: {
@@ -19,7 +31,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body className={monaspacekrypton.className}>
         <ThemeProvider>
             <Header />
             <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white transition-colors duration-300">
